8c7db95e1f9a8c15397b40b3b028e57b513b4dd8
JackieOuzman <jackie.ouzman@csiro.au>
2023-06-22 10:16:08 +0930

    updating with Chiswick data

--- step 3 clipping the aniamls logs to bounadries.R
+++ step 3 clipping the aniamls logs to bounadries.R
@@ -14,12 +14,17 @@ library(sf)
 ############                  bring in boundaries             ##############################
 ############################################################################################
 
-hard_fence_bound <- st_read("W:/VF/Sheep_Lameroo_2022/spatial_boundary/VF_working/HF_Lameroo_rough_proj.shp")  # this is the hard fences
-VF <- st_read("W:/VF/Sheep_Lameroo_2022/spatial_boundary/VF_proj.shp")
+hard_fence_bound <- st_read("W:/VF/Sheep_Chiswick_2022/spatial_boundaries/Chiswick_paddock_boundary_final.shp")  # this is the hard fences
+hard_fence_bound <-
+  st_transform(hard_fence_bound, crs = 28355)
+
+hard_fence_bound_buff <- st_read("W:/VF/Sheep_Chiswick_2022/spatial_boundaries/Chiswick_paddock_boundary_final_buff10.shp")  # this is the 
+hard_fence_bound_buff <-
+  st_transform(hard_fence_bound_buff, crs = 28355)
 
 
-hard_fence_bound <-  st_transform(hard_fence_bound, crs = 28354)
-VF <-  st_transform(VF, crs = 28354)
+VF <-   st_read("W:/VF/Sheep_Chiswick_2022/spatial_boundaries/VF_paddock.shp")
+VF <-  st_transform(VF_paddock, crs = 28355)
 
 ############################################################################################
 
@@ -31,12 +36,12 @@ VF <-  st_transform(VF, crs = 28354)
 
 
 
-step1_2 <- read_csv("W:/VF/Optimising_VF/Lameroo/data_prep/Step1b_animals_GPS_trim_time.csv")
+step1_2 <- read_csv("W:/VF/Optimising_VF/Chiswick/data_prep/Step1b_animals_GPS_trim_time.csv")
 
 #turn into spatial data
 step1_2_sf <-   st_as_sf(step1_2,
                        coords = c("X", "Y"),
-                       crs = 28354, 
+                       crs = 28355, 
                        agr = "constant")
 
 
@@ -45,17 +50,18 @@ step1_2_sf <-   st_as_sf(step1_2,
 
 
 
-#To the large block boundary
+#To the large block boundary with buffer
 step1_2_sf_clip <-
-  st_intersection(step1_2_sf, st_difference(hard_fence_bound)) #this 'st_difference' function is supposed to remove the duplication
+  st_intersection(step1_2_sf, st_difference(hard_fence_bound_buff)) #this 'st_difference' function is supposed to remove the duplication
 
 
-
+##--- up to here---###
 
 ### check
 
 ggplot() +
   geom_sf(data = hard_fence_bound, color = "black", fill = NA) +
+  geom_sf(data = hard_fence_bound_buff, color = "black", fill = NA) +
   geom_sf(data = VF, color = "black", fill = NA) +
   geom_sf(data = step1_2_sf_clip ,alpha = 0.03) +
   theme_bw()+
@@ -81,7 +87,7 @@ step1_2_sf_clip_df <-   cbind(step1_2_sf_clip_df,coordinates )
 
 
 
-path_output_files <- "W:/VF/Optimising_VF/Lameroo/data_prep/" 
+path_output_files <- "W:/VF/Optimising_VF/Chiswick/data_prep/" 
 path_output_files
 write.csv(step1_2_sf_clip_df, 
           paste0(path_output_files,"/step3_clip.csv"), 
