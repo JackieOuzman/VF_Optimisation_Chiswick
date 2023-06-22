5e971ea772e5b95b9e7238ece63fd4cb3bb1f7db
JackieOuzman <jackie.ouzman@csiro.au>
2023-06-22 15:53:15 +0930

    merging the regular time step data with chiswisk data and paths

---
+++ step 5c merge5and5b.R
@@ -0,0 +1,30 @@
+library(dplyr)
+library(tidyverse)
+library(readr)
+library(lubridate)
+library(DT)
+library(sp)
+#install.packages("sf")
+library(sf)
+
+################################################################################
+### Merge step 5 and step 5b 
+################################################################################
+
+step5 <- read_csv("W:/VF/Optimising_VF/Chiswick/data_prep/step5_Greg_time_step_dist_travelled.csv")
+step5b <- read_csv("W:/VF/Optimising_VF/Chiswick/data_prep/step5b_Greg_time_step_dist_travelled_withCue.csv")
+
+str(step5)
+str(step5b)
+
+step5b <- step5b %>% 
+  dplyr::select(Time_sheep,cumulativeAudioCount:Shock_values)
+
+
+all_step5 <- left_join(step5, step5b)
+
+
+output_path <- "W:/VF/Optimising_VF/Chiswick/data_prep/"  #animals_GPS_trim_time
+write.csv(all_step5, 
+          paste0(output_path,"/step5_all.csv"), 
+          row.names=FALSE)
